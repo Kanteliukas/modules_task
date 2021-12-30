e8cc0f138b30edd84e0823960af7cf6f688eef69
Dainius Sabaras <dainius.sabaras@gmail.com>
2021-12-30 21:27:44 +0200

    ikeliama uzduotis

---
+++ main.py
@@ -0,0 +1,13 @@
+from module import function
+from module import Object
+from module import variable
+
+def main():
+    print(variable)
+    function()
+    Object()
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ module.py
@@ -0,0 +1,9 @@
+
+variable = f"This is variable"
+
+def function():
+    print(f"This is function")
+
+class Object():
+    def __init__(self):
+        print(f"This is class")
